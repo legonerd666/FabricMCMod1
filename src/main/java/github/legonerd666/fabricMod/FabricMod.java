1b07d5ecc1a162ca22ab1c37e2875ec06c1f7b01
legonerd666 <chipman.liam@gmail.com>
2023-02-05 20:24:48 -0500

    Added main file and adjusted mod settings

---
+++ src/main/java/github/legonerd666/fabricMod/FabricMod.java
@@ -0,0 +1,12 @@
+package github.legonerd666.fabricMod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricMod implements ModInitializer{
+
+    @Override
+    public void onInitialize() {
+        System.out.println("I am running!");
+    }
+    
+}
